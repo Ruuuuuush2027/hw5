75c738f234c33f4ba0f9c3e4e63e6e08d798e878
Maohe Jiang <mojiang@usc.edu>
2024-04-15 21:07:10 -0700

    schedule work done

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,7 +21,7 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+void schedule_helper(size_t curr_day, bool& found, vector<int>& shifts, const AvailabilityMatrix& avail, const size_t d, DailySchedule& sched);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -36,10 +36,39 @@ bool schedule(
         return false;
     }
     sched.clear();
-    // Add your code below
+    // Create Possible Shifts
+    vector<int> shifts;
+    for(size_t i = 0; i < avail.size(); i ++){
+        shifts.push_back(maxShifts);
+        sched.push_back(std::vector<Worker_T>());
+    }
 
-
-
-
+    bool found = false;
+    schedule_helper(0, found, shifts, avail, dailyNeed, sched);
+    return found;
 }
 
+void schedule_helper(size_t curr_day, bool& found, vector<int>& shifts, const AvailabilityMatrix& avail, const size_t d, DailySchedule& sched){
+    if(found)
+        return;
+    else if(curr_day == avail.size()){
+        found = true;
+        return;
+    }
+    else if(sched[curr_day].size() == d)
+        schedule_helper(curr_day + 1, found, shifts, avail, d, sched);
+    else{
+        Worker_T start = sched[curr_day].empty() ? 0 : (sched[curr_day].back() + 1);
+        for(Worker_T i = start; i < avail[curr_day].size(); i ++){
+            if(avail[curr_day][i] && shifts[i] > 0){
+                sched[curr_day].push_back(i);
+                shifts[i] --;
+                schedule_helper(curr_day, found, shifts, avail, d, sched);
+                if(found)
+                    return;
+                sched[curr_day].pop_back();
+                shifts[i] ++;
+            }
+        }
+    }
+}
