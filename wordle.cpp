c6bfb1ac1491412a8f04e99120fd765a6147d0f3
Maohe Jiang <mojiang@usc.edu>
2024-04-15 20:08:29 -0700

    done wordle

--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,7 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void wordle_helper(const string& in, string floating, const set<string>& dict, size_t emp, string curr, set<string>& target);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +22,43 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+    
+    set<string> target;
 
+    // count number of empty spaces
+    size_t emp = 0;
+    for(size_t i = 0; i < in.size(); i ++){
+        if(in[i] == '-')
+            emp ++;
+    }
+
+    wordle_helper(in, floating, dict, emp, "", target);
+
+    return target;
 }
 
 // Define any helper functions here
+void wordle_helper(const string& in, string floating, const set<string>& dict, size_t emp, string curr, set<string>& target){
+    if(emp < floating.size())
+        return;
+    else if(curr.size() == in.size()){
+        if(dict.find(curr) != dict.end() && floating.empty())
+            target.insert(curr);
+    }
+    else if(in[curr.size()] != '-')
+        wordle_helper(in, floating, dict, emp, curr + in[curr.size()], target);
+    else{
+        emp --;
+        for(int i = 0; i < 26; i ++){
+            char c = char(97 + i);
+            size_t loc = floating.find(c);
+            if(loc != std::string::npos){
+                floating.erase(loc, 1);
+                wordle_helper(in, floating, dict, emp, curr + c, target);
+                floating += c;
+            } else{
+                wordle_helper(in, floating, dict, emp, curr + c, target);
+            }
+        }
+    }
+}
